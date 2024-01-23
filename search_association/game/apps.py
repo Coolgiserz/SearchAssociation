20349e121a9ebd482e77c0982c69cc24f5228f1e
weirdgiser <0106160226@csu.edu.cn>
2024-01-24 00:38:10 +0800

    feat: 游戏名联想支持倒排索引模式

--- search_association/game/apps.py
+++ search_association/game/apps.py
@@ -1,8 +1,11 @@
 import os
+import time
 from django.apps import AppConfig
 from django.conf import settings
 from utils.trie.struct_ import TrieTree
-
+from utils.index.handlers.word import WordSegIndexHandler, WordLetterIndexHandler
+from utils.index.persist import SimpleMemoryIndexPersist
+from utils.index.indexers import IndexerProxy
 game_name_association_flag_string = "ASSOCIATION_FLAG_STRING"
 
 class GameConfig(AppConfig):
@@ -15,7 +18,7 @@ class GameConfig(AppConfig):
         :return:
         """
         # TODO： 初始化完之后对Trie树进行持久化存储
-        print("初始化联想算法...")
+        print("初始化Game联想算法...")
 
         if os.environ.get("RUN_MAIN"):
             # 无需使用noreload标志启动server即可避免ready方法执行两次
@@ -30,12 +33,35 @@ class GameConfig(AppConfig):
                 if not flag:
                     # https://docs.djangoproject.com/zh-hans/4.2/ref/applications/
                     TGame = self.get_model("TGame")
-                    game_names_obj = TGame.objects.values("name_zh").all()
-                    tree = TrieTree()
-                    for name in game_names_obj:
-                        tree.insert(name.get("name_zh"))
-                    setattr(settings, "game_name_association_trie", tree)
-                    print("联想算法初始化完成！")
+                    self.game_names_obj = TGame.objects.values("name_zh").all()
+                    self.init_trie_tree()
+                    self.init_index_proxy()
+
+    def init_trie_tree(self):
+        tree = TrieTree()
+        for name in self.game_names_obj:
+            tree.insert(name.get("name_zh"))
+        setattr(settings, "game_name_association_trie", tree)
+        print("Game-Trie树初始化完成！")
+
+    def init_index_proxy(self):
+        """
+        初始化倒排索引代理
+        :return:
+        """
+        start_time = time.time()
+        index_proxy = IndexerProxy()
+        word_letter_handler = WordLetterIndexHandler()
+        word_seg_handler = WordSegIndexHandler()
+        index_proxy.add_handler(word_seg_handler, word_letter_handler)
+        for name in self.game_names_obj:
+            index_proxy.process(name.get("name_zh"))
+        index_proxy.persist.index_construction_trigger()
+        setattr(settings, "game_name_association_index_proxy", index_proxy)
+        end_time = time.time()
+        print("time cost: ", end_time - start_time)
+        print(f"Game-倒排索引初始化完成！Cost: {end_time - start_time}")
+
 
 
 
--- search_association/game/views.py
+++ search_association/game/views.py
@@ -4,21 +4,26 @@ from django.conf import settings
 from .models import TGame
 from utils import response
 from utils.trie.struct_ import TrieTree
+
+
 # Create your views here.
 
 class AssociativeSearchView(View):
     def get(self, request):
         # params = requests.param_dict()
         keyword = request.GET.get("keyword")
-        query_type = request.GET.get("type")
-
+        query_type = request.GET.get("type", '1')
         print(request.GET.get("keyword"))
         if hasattr(settings, "game_name_association_trie"):
             assert isinstance(settings.game_name_association_trie, TrieTree)
         else:
             return http.JsonResponse(status=500, data={"errmsg": "settings file has no game_name_association_trie"})
         # print(settings.game_name_association_trie.root.children)
-        data = settings.game_name_association_trie.startwith(keyword)
+        data = []
+        if query_type == '1':
+            data = settings.game_name_association_trie.startwith(keyword)
+        elif query_type == '2':
+            data = settings.game_name_association_index_proxy.search_word_with_suggest(keyword)
 
         return http.JsonResponse(data=response.make_success_response(data=sorted(data, key=len)),
                                  json_dumps_params={'ensure_ascii': False})
