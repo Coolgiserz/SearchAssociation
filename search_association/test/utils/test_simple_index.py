5e6fef240903dde2493721a8b67771a56d338483
weirdgiser <0106160226@csu.edu.cn>
2024-01-26 00:16:39 +0800

    test: 新增对Redis索引存储和获取的测试用例

--- search_association/test/utils/test_simple_index.py
+++ search_association/test/utils/test_simple_index.py
@@ -1,12 +1,12 @@
 import unittest
 from utils.index.indexers import SimpleIndexProxy
-
+from utils.index.persist.db import RedisIndexPersist
 
 class SimpleIndexTestCase(unittest.TestCase):
     def setUp(self) -> None:
         pass
 
-    def test_something(self):
+    def test_simple_index_proxy(self):
         cases = ["AI算法", "机器学习", "OpenAI", "openai", "AI框架", "项目投资事件", "数据库安全","小明硕士毕业于中国科学院计算所，后在日本京都大学深造"]
 
         self.proxy = SimpleIndexProxy.init_index_proxy_from_words(cases)
@@ -16,6 +16,16 @@ class SimpleIndexTestCase(unittest.TestCase):
         for query in querys:
             print(f"query {query} ", self.proxy.search_word_with_suggest(query))
 
+    def test_simple_index_proxy_with_redis(self):
+        cases = ["AI算法", "机器学习", "OpenAI", "openai", "AI框架", "项目投资事件", "数据库安全",
+                 "小明硕士毕业于中国科学院计算所，后在日本京都大学深造"]
+
+        self.proxy = SimpleIndexProxy.init_index_proxy_from_words(cases, persist_class=RedisIndexPersist())
+        print("all index: ", self.proxy.get_all_indexes())
+
+        querys = ["j", "J", "A", "ai", "jq", "机"]
+        for query in querys:
+            print(f"query {query} ", self.proxy.search_word_with_suggest(query))
 
 
 if __name__ == '__main__':
